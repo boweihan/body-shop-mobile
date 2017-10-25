a238723f82e9852ea9763d460c90c47bd3012b84
bowei <bowei.han100@gmail.com>
2017-10-25 00:24:30 -0400

    fully imported mike's app into expo (minus RNFB)

--- navigation/RootNavigation.js
+++ navigation/RootNavigation.js
@@ -1,23 +1,43 @@
 import { Notifications } from 'expo';
 import React from 'react';
 import { StackNavigator } from 'react-navigation';
-import MainTabNavigator from './MainTabNavigator';
+// import MainTabNavigator from './MainTabNavigator';
+import LoginScreen from '../screens/mdo/LoginScreen';
+import ReportListScreen from '../screens/mdo/ReportListScreen';
+import ReportScreen from '../screens/mdo/ReportScreen';
+import ReportDetailScreen from '../screens/mdo/ReportDetailScreen';
+import PropertyListScreen from '../screens/mdo/PropertyListScreen';
+import LocationListScreen from '../screens/mdo/LocationListScreen';
+import LocationDetailScreen from '../screens/mdo/LocationDetailScreen';
+import TestScreen from '../screens/mdo/TestScreen';
+
 import registerForPushNotificationsAsync from '../api/registerForPushNotificationsAsync';
 
-const RootStackNavigator = StackNavigator(
-    {
-        Main: {
-            screen: MainTabNavigator,
-        },
-    },
-    {
-        navigationOptions: () => ({
-            headerTitleStyle: {
-                fontWeight: 'normal',
-            },
-        }),
-    },
-);
+const RootStackNavigator = StackNavigator({
+    Home: { screen: LoginScreen },
+    PropertyList: { screen: PropertyListScreen },
+    ReportList: { screen: ReportListScreen },
+    Report: { screen: ReportScreen },
+    ReportDetail: { screen: ReportDetailScreen },
+    LocationList: { screen: LocationListScreen },
+    LocationDetail: { screen: LocationDetailScreen },
+    Test: { screen: TestScreen },
+}, { headerMode: 'screen' });
+
+// const RootStackNavigator = StackNavigator(
+//     {
+//         Main: {
+//             screen: MainTabNavigator,
+//         },
+//     },
+//     {
+//         navigationOptions: () => ({
+//             headerTitleStyle: {
+//                 fontWeight: 'normal',
+//             },
+//         }),
+//     },
+// );
 
 export default class RootNavigator extends React.Component {
     componentDidMount() {
